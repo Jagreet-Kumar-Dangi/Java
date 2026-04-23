916f61d4f017b013ddf4f344753bb6980d8d3953
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-23 22:48:16 +0530

    Print String

---
+++ beggnierjava/string.java
@@ -0,0 +1,16 @@
+package beggnierjava;
+
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();  
+
+        System.out.println("You entered: " + input);
+
+        sc.close();
+    }
+}
