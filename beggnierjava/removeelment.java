edc22a8f1dc12223298980f21571e9e22201289e
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-24 23:14:30 +0530

    Optimal solution

--- beggnierjava/removeelment.java
+++ beggnierjava/removeelment.java
@@ -23,17 +23,35 @@ public class removeelment {
 
     //better approach
 
-    public int removeElement(int[] nums, int val) {
-        int k = 0;
+    // public int removeElement(int[] nums, int val) {
+    //     int k = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (nums[i] != val) {
+    //             nums[k] = nums[i];
+    //             k++;
+    //         }
+    //     }
+
+    //     return k;
+    // }
+
+    //optimal solution
+
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
             }
         }
 
-        return k;
+        return n;
     }
 
 
