5fb8f0bdc36be084effff3730df8ffdf93e7d85d
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-24 23:13:22 +0530

    Better approach

--- beggnierjava/removeelment.java
+++ beggnierjava/removeelment.java
@@ -2,22 +2,39 @@ package beggnierjava;
 
 public class removeelment {
     //Brute Force
-    
+
+    // public int removeElement(int[] nums, int val) {
+    //     int[] temp = new int[nums.length];
+    //     int k = 0;
+
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (nums[i] != val) {
+    //             temp[k++] = nums[i];
+    //         }
+    //     }
+
+
+    //     for (int i = 0; i < k; i++) {
+    //         nums[i] = temp[i];
+    //     }
+
+    //     return k;
+    // }
+
+    //better approach
+
     public int removeElement(int[] nums, int val) {
-        int[] temp = new int[nums.length];
         int k = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                temp[k++] = nums[i];
+                nums[k] = nums[i];
+                k++;
             }
-        }
-
-        // copy back
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
         }
 
         return k;
     }
+
+
 }
