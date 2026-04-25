caf3f2ca315956e29edcd39dbc1656cde07cf5aa
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-25 23:53:37 +0530

    Optimal solution

--- beggnierjava/mergeSorted.java
+++ beggnierjava/mergeSorted.java
@@ -14,23 +14,62 @@ public class mergeSorted {
 
     //     return res;
     // }
-    public static int[] better(int[] a, int[] b) {
-        int n = a.length, m = b.length;
-        int[] res = new int[n + m];
+    // public static int[] better(int[] a, int[] b) {
+    //     int n = a.length, m = b.length;
+    //     int[] res = new int[n + m];
     
-        int i = 0, j = 0, k = 0;
+    //     int i = 0, j = 0, k = 0;
     
-        while (i < n && j < m) {
-            if (a[i] <= b[j]) {
-                res[k++] = a[i++];
-            } else {
-                res[k++] = b[j++];
+    //     while (i < n && j < m) {
+    //         if (a[i] <= b[j]) {
+    //             res[k++] = a[i++];
+    //         } else {
+    //             res[k++] = b[j++];
+    //         }
+    //     }
+    
+    //     while (i < n) res[k++] = a[i++];
+    //     while (j < m) res[k++] = b[j++];
+    
+    //     return res;
+    // }
+
+    public static void optimal(int[] a, int[] b) {
+    int n = a.length, m = b.length;
+    int gap = (n + m + 1) / 2;
+
+    while (gap > 0) {
+        int i = 0;
+        int j = i + gap;
+
+        while (j < n + m) {
+
+            int val1 = (i < n) ? a[i] : b[i - n];
+            int val2 = (j < n) ? a[j] : b[j - n];
+
+            if (val1 > val2) {
+                // swap cases
+                if (i < n && j < n) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                } else if (i < n && j >= n) {
+                    int temp = a[i];
+                    a[i] = b[j - n];
+                    b[j - n] = temp;
+                } else {
+                    int temp = b[i - n];
+                    b[i - n] = b[j - n];
+                    b[j - n] = temp;
+                }
             }
+
+            i++;
+            j++;
         }
-    
-        while (i < n) res[k++] = a[i++];
-        while (j < m) res[k++] = b[j++];
-    
-        return res;
+
+        if (gap == 1) break;
+        gap = (gap + 1) / 2;
     }
+}
 }
