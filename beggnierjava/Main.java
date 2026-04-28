c0ac79ef43f81be1c3dd4c4e44052e8b37f8c5a0
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-28 23:39:07 +0530

    Palindrome

---
+++ beggnierjava/Main.java
@@ -0,0 +1,43 @@
+package beggnierjava;
+import java.util.Scanner;
+
+class palindrome{
+
+    public boolean isPalindrome(int x) {
+
+        // Negative numbers and numbers ending with 0 (except 0) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+
+        // Reverse only half of the number
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Check for palindrome
+        return x == rev || x == rev / 10;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+       palindrome obj = new palindrome();
+        boolean result = obj.isPalindrome(num);
+
+        if (result) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
