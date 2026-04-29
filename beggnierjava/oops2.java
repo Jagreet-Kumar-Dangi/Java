8333df7560e69c0da77a55886d627d6e5497c544
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-29 23:24:04 +0530

    mistake

--- beggnierjava/oops2.java
+++ beggnierjava/oops2.java
@@ -12,7 +12,7 @@ public class oops2 {
         for(int i=0;i<3;i++){
             for(int j=0;j<4;j++){
                 System.out.print(num2[i][j]);
-                System.out.print("\t");
+                System.out.print("\t ");
             }
             System.out.println();
         }
