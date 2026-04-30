da5843577ef737eeb2f100ea3ba0ae629a55d012
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-04-30 23:45:40 +0530

    hi

--- beggnierjava/temp.java
+++ beggnierjava/temp.java
@@ -2,6 +2,6 @@ package beggnierjava;
 
 public class temp {
     public static void main(String[] args) {
-        System.out.println("hello java");
+        System.out.println("Bye java");
     }
 }
