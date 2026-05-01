ac9b2a1553d85f817620e728119c7b1e1b61a1f1
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-05-01 23:43:08 +0530

    dot

--- beggnierjava/stringbuffer.java
+++ beggnierjava/stringbuffer.java
@@ -2,7 +2,7 @@ package beggnierjava;
 
 public class stringbuffer {
     public static void main(String[] args) {
-        StringBuffer sb=new StringBuffer("Jagreet");
+        StringBuffer sb=new StringBuffer("Jagreet ");
         sb.append(" Dangi");
         sb.deleteCharAt(4);
         System.out.println(sb);
