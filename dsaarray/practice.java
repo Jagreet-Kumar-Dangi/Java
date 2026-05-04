f1a19305fc1d169f7f6c188ccde989044d06815a
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-05-04 23:37:05 +0530

    hi

--- dsaarray/practice.java
+++ dsaarray/practice.java
@@ -44,7 +44,7 @@ public class practice {
         System.out.print("Enter length of the array: ");
         int n=sc.nextInt();
         int arr[]=new int[n];
-        System.out.println("Enter the element of an array: ");
+        System.out.println("Enter the element of an array- ");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
