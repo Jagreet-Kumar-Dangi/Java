0c97dd8662bf19a69660643324ebfd6c0a677372
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-05-04 23:36:20 +0530

    nothing

--- dsaarray/leftrotate.java
+++ dsaarray/leftrotate.java
@@ -64,7 +64,7 @@ public class leftrotate {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the length of the array ");
         int n=sc.nextInt();
-        System.out.println("Enter the array elements: ");
+        System.out.println("Enter the array element ");
         int[] arr=new int[n];
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
