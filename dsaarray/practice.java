0415a1bc66530d95cd897e149dd713cddf7e5aa9
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-05-04 23:37:25 +0530

    not

--- dsaarray/practice.java
+++ dsaarray/practice.java
@@ -41,7 +41,7 @@ public class practice {
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter length of the array: ");
+        System.out.print("Enter length of the array- ");
         int n=sc.nextInt();
         int arr[]=new int[n];
         System.out.println("Enter the element of an array- ");
