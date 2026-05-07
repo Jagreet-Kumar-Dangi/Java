816672e62966fcccc6fbb6b7be9a849f8ae28109
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-05-07 23:00:25 +0530

    perfect numebr.

---
+++ beggnierjava/perfectnum.java
@@ -0,0 +1,29 @@
+package beggnierjava;
+
+import java.util.Scanner;
+
+public class perfectnum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n, sum = 0;
+
+        System.out.println("Enter a number:");
+        n = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+
+        sc.close();
+    }
+}
